84f44059b8d82612276892d056e36497a0e1c742
dimitriandre <dimitri@atmosclear.com>
2023-02-13 17:42:46 +0100

    model category

---
+++ ThriftshopRazorWeb/Model/Class.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThriftshopRazorWeb.Model
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
